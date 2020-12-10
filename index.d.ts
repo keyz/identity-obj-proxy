f246944eedb19859c1cf3bfb37a9b7d89873af93
Remco Haszing <remcohaszing@gmail.com>
2020-12-10 14:34:46 +0100

    Add TypeScript type definitions
    This allows the package to be consumed using TypeScript without any additional
    configuration.

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * An identity object using ES6 proxies. Useful for mocking webpack imports. For instance, you can
+ * tell Jest to mock this object as imported
+ * [CSS modules](https://github.com/css-modules/css-modules); then all your `className` lookups on
+ * the imported `styles` object will be returned as-is.
+ */
+declare const idObj: Record<string, string>;
+
+export = idObj
